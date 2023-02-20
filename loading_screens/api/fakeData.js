b3ba9bf0866304b79aa63041d34a7b4a314017f3
Faizee095 <faiyazsiddique9@gmail.com>
2023-02-20 12:08:17 +0530

    loading screens

---
+++ loading_screens/api/fakeData.js
@@ -0,0 +1,115 @@
+const user = {
+    id: 1,
+    username: "john",
+    avatar:
+      "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  };
+  
+  const videos = [
+    {
+      id: 1,
+      title: "React.js: The Documentary",
+      views: "269k",
+      date: "1 week ago",
+      duration: "1:18:14",
+      img: "https://cdn.pimylifeup.com/wp-content/uploads/2022/08/javascript-console.log-function-Thumbnail-NoWM.png",
+      channel: {
+        name: "Amazing Developer",
+        avatar:
+          "https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      },
+    },
+    {
+      id: 2,
+      title: "React.js Full Course",
+      views: "1.1M",
+      date: "2 weeks ago",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJm8NdHjGES8j9Dm410u5RaKrLmkjPjyobrQ&usqp=CAU",
+      duration: "11:00",
+      channel: {
+        name: "Nice Dev",
+        avatar:
+          "https://images.pexels.com/photos/936019/pexels-photo-936019.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      },
+    },
+    {
+      id: 3,
+      title: "A Day in the life of a SDE",
+      views: "420K",
+      date: "2 days ago",
+      img: "https://i.ytimg.com/vi/eQBvBtJd6wE/maxresdefault.jpg",
+      duration: "8:00:00",
+      channel: {
+        name: "It Professor",
+        avatar:
+          "https://images.pexels.com/photos/3149036/pexels-photo-3149036.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      },
+    },
+    {
+      id: 4,
+      title: "If you do this, You are not a developer!",
+      views: "820K",
+      date: "1 week ago",
+      img: "https://i.ibb.co/ZMFYyst/4.png",
+      duration: "14:00",
+      channel: {
+        name: "Codeloper",
+        avatar:
+          "https://images.pexels.com/photos/2659177/pexels-photo-2659177.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      },
+    },
+    {
+      id: 5,
+      title: "Build an app without code and sell it",
+      views: "134K",
+      date: "1 day ago",
+      img: "https://i.ibb.co/0n3qXLk/5.png",
+      duration: "5:17",
+      channel: {
+        name: "Codeinvestor",
+        avatar:
+          "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      },
+    },
+    {
+      id: 6,
+      title: "People think I'm a master coder!",
+      views: "13M",
+      date: "3 days ago",
+      img: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/coding-design-template-7fdcbfed1fb311be96a282920fba8515_screen.jpg?ts=1599184512",
+      duration: "06:09",
+      channel: {
+        name: "Devrella",
+        avatar:
+          "https://images.pexels.com/photos/4348798/pexels-photo-4348798.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      },
+    },
+    {
+      id: 7,
+      title: "My thoughts as an ex Amazon CEO",
+      views: "3.1M",
+      date: "1 month ago",
+      img: "https://i.ibb.co/g7j1MBY/7.png",
+      duration: "23:15",
+      channel: {
+        name: "Technopat",
+        avatar:
+          "https://images.pexels.com/photos/7594067/pexels-photo-7594067.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      },
+    },
+    {
+      id: 8,
+      title: "Do These! If you want to be a developer",
+      views: "92K",
+      date: "5 days ago",
+      img: "https://i.ibb.co/dKCBD0r/8.png",
+      duration: "2:56",
+      channel: {
+        name: "Coder Hodor",
+        avatar:
+          "https://images.pexels.com/photos/7610757/pexels-photo-7610757.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      },
+    },
+  ];
+  
+  module.exports = { user, videos };
