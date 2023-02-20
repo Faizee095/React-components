dc640f2e68dcf7ebf1a4773ec0d42f711ce42cb5
Faizee095 <faiyazsiddique9@gmail.com>
2023-02-20 13:07:11 +0530

    Skeleton loaders

---
+++ skeleton_loaders/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Post from "./components/Post";
+import { SkeletonTheme } from "react-loading-skeleton";
+
+function App() {
+  return (
+    <div className="App">
+      <SkeletonTheme baseColor="#313131" highlightColor="#525252">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/post/:userId" element={<Post />} />
+          </Routes>
+        </BrowserRouter>
+      </SkeletonTheme>
+    </div>
+  );
+}
+
+export default App;
