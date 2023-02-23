633a2ecb4c08613fd5d06eaead48976a6cc13ff1
Faizee095 <faiyazsiddique9@gmail.com>
2023-02-23 12:39:12 +0530

    multi table row search

--- search/client/src/App.js
+++ search/client/src/App.js
@@ -1,9 +1,40 @@
 import { useState } from "react";
 import "./App.css";
+import Table from "./Table";
 import { Users } from "./users";
+
+// function App() {
+//   const [query, setQuery] = useState("");
+
+//   return (
+//     <div className="app">
+//       <input
+//         type="text"
+//         className="search"
+//         placeholder="Search..."
+//         onChange={(e) => setQuery(e.target.value)}
+//       />
+//       <ul className="list">
+//         {Users.filter((user) =>
+//           user.first_name.toLowerCase().includes(query.toLowerCase())
+//         ).map((user) => (
+//           <li key={user.id} className="listItem">
+//             {user.first_name}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
 
 function App() {
   const [query, setQuery] = useState("");
+  const keys = ["first_name", "last_name", "email","gender"];
+  const search = (data) => {
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(query.toLowerCase()))
+    );
+  };
 
   return (
     <div className="app">
@@ -13,15 +44,7 @@ function App() {
         placeholder="Search..."
         onChange={(e) => setQuery(e.target.value)}
       />
-      <ul className="list">
-        {Users.filter((user) =>
-          user.first_name.toLowerCase().includes(query.toLowerCase())
-        ).map((user) => (
-          <li key={user.id} className="listItem">
-            {user.first_name}
-          </li>
-        ))}
-      </ul>
+      {<Table data={search(Users)}/>}
     </div>
   );
 }
