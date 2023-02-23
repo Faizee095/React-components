2ef6d827fa22d7d7b0102b7671ada868d76a4a0c
Faizee095 <faiyazsiddique9@gmail.com>
2023-02-23 15:27:45 +0530

    Search with backend

--- search/client/src/App.js
+++ search/client/src/App.js
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState , useEffect } from "react";
 import "./App.css";
 import Table from "./Table";
 import { Users } from "./users";
+import axios from 'axios'
 
 // function App() {
 //   const [query, setQuery] = useState("");
@@ -27,24 +28,48 @@ import { Users } from "./users";
 //   );
 // }
 
+// function App() {
+//   const [query, setQuery] = useState("");
+//   const keys = ["first_name", "last_name", "email","gender"];
+//   const search = (data) => {
+//     return data.filter((item) =>
+//       keys.some((key) => item[key].toLowerCase().includes(query.toLowerCase()))
+//     );
+//   };
+
+//   return (
+//     <div className="app">
+//       <input
+//         type="text"
+//         className="search"
+//         placeholder="Search..."
+//         onChange={(e) => setQuery(e.target.value)}
+//       />
+//       {<Table data={search(Users)}/>}
+//     </div>
+//   );
+// }
+
 function App() {
   const [query, setQuery] = useState("");
-  const keys = ["first_name", "last_name", "email","gender"];
-  const search = (data) => {
-    return data.filter((item) =>
-      keys.some((key) => item[key].toLowerCase().includes(query.toLowerCase()))
-    );
-  };
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(`http://localhost:5000?q=${query}`);
+      setData(res.data);
+    };
+    if (query.length === 0 || query.length > 2) fetchData();
+  }, [query]);
 
   return (
     <div className="app">
       <input
-        type="text"
         className="search"
         placeholder="Search..."
-        onChange={(e) => setQuery(e.target.value)}
+        onChange={(e) => setQuery(e.target.value.toLowerCase())}
       />
-      {<Table data={search(Users)}/>}
+      {<Table data={data} />}
     </div>
   );
 }
